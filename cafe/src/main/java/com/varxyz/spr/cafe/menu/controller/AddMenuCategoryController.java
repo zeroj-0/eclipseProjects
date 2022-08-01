a92d20c6a17a098af43ecbafecef4f0b3b297add
zeroj-0 <syj990930@naver.com>
2022-08-01 17:56:53 +0900

    220801

--- cafe/src/main/java/com/varxyz/spr/cafe/menu/controller/AddMenuCategoryController.java
+++ cafe/src/main/java/com/varxyz/spr/cafe/menu/controller/AddMenuCategoryController.java
@@ -1,5 +1,15 @@
 package com.varxyz.spr.cafe.menu.controller;
 
-public class AddMenuCategoryController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller("menu.controller.addMenuCategoryController")
+public class AddMenuCategoryController {
+	
+	@GetMapping("/add_menuCategory")
+	public String addCategoryForm(Model model) {
+		model.addAttribute("menuCategory", new MenuCategoryCommand());
+		return "cafe/add_menuCategory";
+	}
 }
--- cafe/src/main/java/com/varxyz/spr/cafe/menu/controller/MenuCategoryCommand.java
+++ cafe/src/main/java/com/varxyz/spr/cafe/menu/controller/MenuCategoryCommand.java
@@ -1,5 +1,5 @@
 package com.varxyz.spr.cafe.menu.controller;
 
 public class MenuCategoryCommand {
-
+	private String name;
 }
